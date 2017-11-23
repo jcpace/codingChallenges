f245481c250dec3d24a0988afb5f659c0c56e28f
Joshua Pace <joshpace@gmail.com>
2017-11-23 11:55:50 -0800

    [add] isPalindrome

---
+++ isPalindrome.js
@@ -0,0 +1,10 @@
+const isPalindrome = (str) => {
+// split the string into words in an array, remove the white spaces, join all char into one string block, and make all char lowercase
+  let other = str.split(' ').join('').toLowerCase()
+// take previous variable, split into array, reverse order, and return to string
+  let reversed = other.split('').reverse().join('')
+
+  return other === reversed
+}
+console.log(isPalindrome('A nut for a jar of tuna'))
+console.log(isPalindrome('racecar'))
