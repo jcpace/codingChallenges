5727d2db8f05bfeda3aec0d424a1e438acd7bfc2
Joshua Pace <joshpace@gmail.com>
2017-12-21 15:57:34 -0800

    [add] simpleAdd

---
+++ simpleAdd.js
@@ -0,0 +1,15 @@
+// PROMPT: Have the function simpleAdd(num) add up all the numbers from 1 to num.
+// For example: if the input is 4 then your program should return 10 because 1 + 2 + 3 + 4 = 10.
+// For the test cases, the parameter num will be any number from 1 to 1000.
+
+const simpleAdd = (num) => {
+  let n = 0
+  for (let i = 0; i <= num; i++) {
+    n += i
+  }
+  return n
+}
+
+console.log(simpleAdd(4))
+console.log(simpleAdd(1))
+console.log(simpleAdd(1000))
