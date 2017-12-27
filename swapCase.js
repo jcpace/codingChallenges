c7db96894d593da244f56fcbd32a6603e518ac60
Joshua Pace <joshpace@gmail.com>
2017-12-27 12:45:50 -0800

    [update] swapCase

--- swapCase.js
+++ swapCase.js
@@ -2,15 +2,17 @@
 // For example: if str is "Hello World" the output should be hELLO wORLD. Let numbers and symbols stay the way they are.
 
 const swapCase = (str) => {
-  let arr = str.split('')
-  return arr.map(function (char) {
-    if (char === char.toLowerCase()) {
-      return char.toUpperCase()
-    }
-    if (char === char.toUpperCase()) {
-      return char.toLowerCase()
-    }
-  }).join('')
+  return str
+    .split('')
+    .map(function (char) {
+      if (char === char.toLowerCase()) {
+        return char.toUpperCase()
+      }
+      if (char === char.toUpperCase()) {
+        return char.toLowerCase()
+      }
+    })
+    .join('')
 }
 
 console.log(swapCase('Hello World'))
