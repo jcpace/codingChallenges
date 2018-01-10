aacfc6d80e227668d6e39b731c06287b22e6a225
Joshua Pace <joshpace@gmail.com>
2018-01-10 10:41:49 -0800

    [add] singly linked list

---
+++ singlyLinkedList.js
@@ -0,0 +1,15 @@
+// PROMPt: Implement a linked list. A singly-linked list is a data structure that holds a sequence of linked nodes.
+// Each node, in turn, contains data and a pointer, which can point to another node.
+
+const Node = data => {
+  this.data = data
+  this.next = null
+}
+
+const linkedList = () => {
+  this.length = 0
+  // number of nodes in the Linked List
+  this.head = null
+  // a pointer to the first node at the front of the list
+}
+
